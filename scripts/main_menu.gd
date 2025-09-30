2ee4047e39abd9bac88a2f1fe9eb3bfd8f8c7ab5
Osaidi1 <hassanosaid123@gmail.com>
2025-09-30 19:38:52 +0200

    fixed potion counter nad health bar ui not working

---
+++ scripts/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+const LEVEL_1 = preload("res://scenes/Level1.tscn") as PackedScene
+@onready var timer: Timer = $Timer
+
+func play():
+	Scenetransition.change_scene()
+	await get_tree().create_timer(1.5).timeout
+	get_tree().change_scene_to_packed(LEVEL_1)
+	Scenetransition.end_transition()
+
+func quit():
+	get_tree().quit()
